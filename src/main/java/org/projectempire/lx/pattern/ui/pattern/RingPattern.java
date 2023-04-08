4c52de32c589762948628ce95841ee8ed54253eb
willivolt <willivolt@user.noreply.github.com>
2023-04-08 16:08:41 -0700

    Counter rotating ring pattern

--- src/main/java/org/projectempire/lx/pattern/ui/pattern/RingPattern.java
+++ src/main/java/org/projectempire/lx/pattern/ui/pattern/RingPattern.java
@@ -13,6 +13,7 @@ import heronarts.lx.utils.LXUtils;
 
 @LXCategory("Nozzle")
 public class RingPattern extends LXPattern {
+    private static final int NUM_BANDS = 5;
     public final ColorParameter color = new ColorParameter("Color").setDescription("Color of the pattern");
     public final CompoundParameter speed = new CompoundParameter("Speed", 1000, 500, 5000);
 
@@ -32,11 +33,13 @@ public class RingPattern extends LXPattern {
 
     @Override
     protected void run(double deltaMs) {
-        // TODO!
         double rot = rotation.getValue();
         float offset = (float)(rot * (Math.PI/180));
         for (LXPoint p : model.points) {
-            float b = Math.abs(LXUtils.sinf((offset + p.theta) / 2f));
+            int band = (int)(p.zn / (1f / (float)(NUM_BANDS - 1)));
+            int sign = (band % 2 == 0) ? -1 : 1;
+            float b = Math.abs(LXUtils.sinf(((sign * offset) + p.theta) / 1f));
+            //b = Math.abs(LXUtils.trif((sign * offset) + (p.theta / LX.TWO_PIf)));
             colors[p.index] = LX.hsb(color.hue.getValuef(), color.saturation.getValuef(), b * 100);
         }
     }
