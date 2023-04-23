dc92626593540ea89f3f90aec55f06f4f1619234
willivolt <willivolt@user.noreply.github.com>
2023-04-23 16:59:07 -0700

    Continued work on Butt Fixtures

--- src/main/java/org/projectempire/lx/structure/ButtBottomFixture.java
+++ src/main/java/org/projectempire/lx/structure/ButtBottomFixture.java
@@ -4,12 +4,14 @@ import heronarts.lx.LX;
 import heronarts.lx.model.LXPoint;
 import heronarts.lx.parameter.BoundedParameter;
 import heronarts.lx.parameter.DiscreteParameter;
+import heronarts.lx.parameter.EnumParameter;
 import heronarts.lx.parameter.LXParameter;
 import heronarts.lx.structure.LXBasicFixture;
 import heronarts.lx.studio.LXStudio;
 import heronarts.lx.studio.ui.fixture.UIFixture;
 import heronarts.lx.studio.ui.fixture.UIFixtureControls;
 import heronarts.lx.transform.LXMatrix;
+import heronarts.lx.transform.LXTransform;
 import heronarts.p4lx.ui.UI2dComponent;
 
 import java.util.List;
@@ -18,6 +20,22 @@ import java.util.Map;
 public class ButtBottomFixture extends LXBasicFixture implements UIFixtureControls<ButtBottomFixture> {
     public static final int MAX_POINTS = 4096;
     public static final float CONTROL_WIDTH = 41.0F;
+
+    public enum ButtMode {
+        WIDE("Wide"),
+        TALL("Tall");
+
+        private final String label;
+
+        ButtMode(String label) {
+            this.label = label;
+        }
+
+        @Override
+        public String toString() {
+            return this.label;
+        }
+    }
 
     public final DiscreteParameter numPoints = (DiscreteParameter)
             new DiscreteParameter("Num", 30, 1, MAX_POINTS + 1)
@@ -32,53 +50,88 @@ public class ButtBottomFixture extends LXBasicFixture implements UIFixtureContro
             new BoundedParameter("Height", 5, 0, 1000000)
                     .setDescription("Height of the fixture in inches");
 
+    public final EnumParameter<ButtMode> buttMode =
+            new EnumParameter<ButtMode>("Mode", ButtMode.WIDE)
+                    .setDescription("Whether the arc wide or tall");
+
     public ButtBottomFixture(LX lx) {
         super(lx, "ButtBottom");
         addMetricsParameter("numPoints", this.numPoints);
         addGeometryParameter("width", this.width);
         addGeometryParameter("height", this.height);
+        addGeometryParameter("buttMode", this.buttMode);
     }
 
     @Override
-    protected void computePointGeometry(LXMatrix transform, List<LXPoint> points) {
+    protected void computePointGeometry(LXMatrix matrix, List<LXPoint> points) {
         // Length of each curve = 1/2 * pi * height, total of curves => pi * height
         // Length of line segment = width - 2 * height
         // Total length = pi * height + width - 2 * height => (pi - 2) * height + width
-        final float radius = this.height.getValuef();
-        float curveLength = LX.HALF_PIf * radius;
-        float lineLength = this.width.getValuef() - (2f * radius);
-        float totalLength = (2f * curveLength) + lineLength;
-        float spacing = totalLength / this.numPoints.getValuef();
-        int firstCurvePoints = (int)(curveLength / spacing);
-        int remainingPoints = this.numPoints.getValuei() - firstCurvePoints;
-        int linePoints = (int)(lineLength / spacing);
-        int secondCurvePoints = remainingPoints - linePoints;
-        float rotation = LX.HALF_PIf / firstCurvePoints;
-        int i = 1;
-        // First 1/4 circle
-        for (LXPoint p: points.subList(0, firstCurvePoints)) {
-            p.set(transform);
-            LX.log(String.format("Point %d: %f, %f", i++, p.x, p.y));
-            transform.translateX(-radius);
-            transform.rotateZ(-rotation);
-            transform.translateX(radius);
-        }
-        // Line segment
-        //transform.translateY(radius);
-        for (LXPoint p: points.subList(firstCurvePoints, firstCurvePoints + linePoints)) {
-            p.set(transform);
-            LX.log(String.format("Point %d: %f, %f", i++, p.x, p.y));
-            transform.translateY(-spacing);
-        }
-        //transform.translateY(-radius);
-        // Second 1/4 circle
-        rotation = LX.HALF_PIf / secondCurvePoints;
-        for (LXPoint p: points.subList(firstCurvePoints + linePoints, points.size())) {
-            p.set(transform);
-            LX.log(String.format("Point %d: %f, %f", i++, p.x, p.y));
-            transform.translateX(-radius);
-            transform.rotateZ(-rotation);
-            transform.translateX(radius);
+        // Spacing = total length / (numPoints - 1)
+        float radius = this.height.getValuef();
+        final float curveLength = LX.HALF_PIf * radius;
+        final float lineLength = this.width.getValuef() - (2f * radius);
+        final float totalLength = (2f * curveLength) + lineLength;
+        final float spacing = totalLength / (this.numPoints.getValuef() - 1);
+        final int firstCurvePoints = (int) (curveLength / spacing);
+        final int linePoints = (int) (lineLength / spacing);
+        final int secondCurvePoints = this.numPoints.getValuei() - (firstCurvePoints + linePoints);
+
+        LXTransform transform = new LXTransform(matrix);
+        switch (this.buttMode.getEnum()) {
+            case WIDE:
+                // We are drawing right to left. Move the view to the top left
+                transform.translateX(-this.width.getValuef() / 2f);
+                transform.push(); // Save origin for later return
+                // First 1/4 circle
+                float rotation = LX.HALF_PIf / firstCurvePoints;
+                // TODO: can we figure out how much unused curve there is?
+                LX.log(String.format("Transform Start 1: %f, %f", transform.x(), transform.y()));
+                for (LXPoint p : points.subList(0, firstCurvePoints)) {
+                    p.set(transform);
+                    transform.translateX(radius);
+                    transform.rotateZ(rotation);
+                    transform.translateX(-radius);
+                }
+                // Line segment
+                // Unlikely that spacing on the line segment will be exact, so move to start of line
+                transform.pop(); // Return to origin
+                transform.push(); // Save again
+                transform.translateX(radius);
+                transform.translateY(-radius);
+                LX.log(String.format("Transform Start 2: %f, %f", transform.x(), transform.y()));
+                if (firstCurvePoints < firstCurvePoints + linePoints) {
+                    for (LXPoint p : points.subList(firstCurvePoints, firstCurvePoints + linePoints)) {
+                        p.set(transform);
+                        transform.translateX(spacing);
+                    }
+                }
+                // Second 1/4 circle
+                transform.pop(); // Return to origin
+                transform.translateX(radius + lineLength);
+                transform.translateY(-radius);
+                rotation = LX.HALF_PIf / secondCurvePoints;
+                LX.log(String.format("Transform Start 3: %f, %f", transform.x(), transform.y()));
+                for (LXPoint p : points.subList(firstCurvePoints + linePoints, this.numPoints.getValuei())) {
+                    p.set(transform);
+                    transform.translateY(radius);
+                    transform.rotateZ(rotation);
+                    transform.translateY(-radius);
+                }
+                break;
+            case TALL:
+                // todo: tall, narrow butt. For now, just a half circle
+                transform.rotateZ(-LX.HALF_PIf);
+                radius = this.width.getValuef() / 2f;
+                rotation = (LX.PIf / (this.points.size() - 1));
+                for (LXPoint p : this.points) {
+                    transform.translateY(-radius);
+                    p.set(transform);
+                    transform.translateY(radius);
+                    transform.rotateZ(rotation);
+                }
+                break;
+
         }
     }
 
@@ -89,7 +142,7 @@ public class ButtBottomFixture extends LXBasicFixture implements UIFixtureContro
 
     @Override
     public String[] getDefaultTags() {
-        return new String[]{"Butt"};
+        return new String[]{"butt"};
     }
 
     @Override
@@ -107,11 +160,13 @@ public class ButtBottomFixture extends LXBasicFixture implements UIFixtureContro
         uiFixture.addSection("B Bottom", new UI2dComponent[][]{
                 {uiFixture.newControlIntBox(buttBottomFixture.numPoints, CONTROL_WIDTH),
                         uiFixture.newControlBox(buttBottomFixture.width, CONTROL_WIDTH),
-                        uiFixture.newControlBox(buttBottomFixture.height, CONTROL_WIDTH)
+                        uiFixture.newControlBox(buttBottomFixture.height, CONTROL_WIDTH),
+                        uiFixture.newControlButton(buttBottomFixture.buttMode, CONTROL_WIDTH)
                 },
                 {uiFixture.newControlLabel("Points", CONTROL_WIDTH),
                         uiFixture.newControlLabel("Width", CONTROL_WIDTH),
-                        uiFixture.newControlLabel("Height", CONTROL_WIDTH)
+                        uiFixture.newControlLabel("Height", CONTROL_WIDTH),
+                        uiFixture.newControlLabel("Mode", CONTROL_WIDTH)
                 }});
         uiFixture.addProtocolSection(buttBottomFixture, true);
     }
