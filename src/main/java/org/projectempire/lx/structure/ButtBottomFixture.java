f65a4efde9a0207556cbdf2908483f27214d29fe
willivolt <willivolt@users.noreply.github.com>
2023-04-23 13:18:00 -0700

    WIP butt bottom

--- src/main/java/org/projectempire/lx/structure/ButtBottomFixture.java
+++ src/main/java/org/projectempire/lx/structure/ButtBottomFixture.java
@@ -20,16 +20,16 @@ public class ButtBottomFixture extends LXBasicFixture implements UIFixtureContro
     public static final float CONTROL_WIDTH = 41.0F;
 
     public final DiscreteParameter numPoints = (DiscreteParameter)
-            new DiscreteParameter("Num", 10, 1, MAX_POINTS + 1)
+            new DiscreteParameter("Num", 30, 1, MAX_POINTS + 1)
                     .setUnits(LXParameter.Units.INTEGER)
                     .setDescription("Number of points in the fixture");
 
     public final BoundedParameter width =
-            new BoundedParameter("Width", 10, 0, 1000000)
+            new BoundedParameter("Width", 15, 0, 1000000)
                     .setDescription("Width of the fixture in inches");
 
     public final BoundedParameter height =
-            new BoundedParameter("Height", 10, 0, 1000000)
+            new BoundedParameter("Height", 5, 0, 1000000)
                     .setDescription("Height of the fixture in inches");
 
     public ButtBottomFixture(LX lx) {
@@ -41,19 +41,44 @@ public class ButtBottomFixture extends LXBasicFixture implements UIFixtureContro
 
     @Override
     protected void computePointGeometry(LXMatrix transform, List<LXPoint> points) {
-        float radius = this.height.getValuef() / 2f;
-        float totalLength = (LX.PIf * radius) + this.width.getValuef() - this.height.getValuef();
-        float spacing = this.numPoints.getValuef() / totalLength;
-        int remainingPoints = this.numPoints.getValuei();
-        int firstCurvePoints = (int) Math.ceil((LX.HALF_PIf * radius) / spacing);
-        remainingPoints -= firstCurvePoints;
-        int linePoints = (int)((this.width.getValuef() - this.height.getValuef()) / spacing);
-        remainingPoints -= linePoints;
-        int secondCurvePoints = remainingPoints;
-        // TODO: Implement this
-        for (LXPoint p : points) {
+        // Length of each curve = 1/2 * pi * height, total of curves => pi * height
+        // Length of line segment = width - 2 * height
+        // Total length = pi * height + width - 2 * height => (pi - 2) * height + width
+        final float radius = this.height.getValuef();
+        float curveLength = LX.HALF_PIf * radius;
+        float lineLength = this.width.getValuef() - (2f * radius);
+        float totalLength = (2f * curveLength) + lineLength;
+        float spacing = totalLength / this.numPoints.getValuef();
+        int firstCurvePoints = (int)(curveLength / spacing);
+        int remainingPoints = this.numPoints.getValuei() - firstCurvePoints;
+        int linePoints = (int)(lineLength / spacing);
+        int secondCurvePoints = remainingPoints - linePoints;
+        float rotation = LX.HALF_PIf / firstCurvePoints;
+        int i = 1;
+        // First 1/4 circle
+        for (LXPoint p: points.subList(0, firstCurvePoints)) {
             p.set(transform);
-            transform.translateX(spacing);
+            LX.log(String.format("Point %d: %f, %f", i++, p.x, p.y));
+            transform.translateX(-radius);
+            transform.rotateZ(-rotation);
+            transform.translateX(radius);
+        }
+        // Line segment
+        //transform.translateY(radius);
+        for (LXPoint p: points.subList(firstCurvePoints, firstCurvePoints + linePoints)) {
+            p.set(transform);
+            LX.log(String.format("Point %d: %f, %f", i++, p.x, p.y));
+            transform.translateY(-spacing);
+        }
+        //transform.translateY(-radius);
+        // Second 1/4 circle
+        rotation = LX.HALF_PIf / secondCurvePoints;
+        for (LXPoint p: points.subList(firstCurvePoints + linePoints, points.size())) {
+            p.set(transform);
+            LX.log(String.format("Point %d: %f, %f", i++, p.x, p.y));
+            transform.translateX(-radius);
+            transform.rotateZ(-rotation);
+            transform.translateX(radius);
         }
     }
 
