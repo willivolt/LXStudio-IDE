26d896b1577c57073d8eafc6a60a5957ca951c93
willivolt <willivolt@users.noreply.github.com>
2023-07-29 12:44:30 -0700

    Ring Pattern updates
    Add pallet option.  Use model children for rings.

--- src/main/java/org/projectempire/lx/pattern/ui/pattern/RingPattern.java
+++ src/main/java/org/projectempire/lx/pattern/ui/pattern/RingPattern.java
@@ -4,18 +4,45 @@ import heronarts.lx.LX;
 import heronarts.lx.LXCategory;
 import heronarts.lx.color.ColorParameter;
 import heronarts.lx.color.LXColor;
+import heronarts.lx.color.LXDynamicColor;
+import heronarts.lx.color.LXSwatch;
+import heronarts.lx.model.LXModel;
 import heronarts.lx.model.LXPoint;
 import heronarts.lx.modulator.SawLFO;
 import heronarts.lx.parameter.CompoundParameter;
+import heronarts.lx.parameter.DiscreteParameter;
+import heronarts.lx.parameter.EnumParameter;
 import heronarts.lx.parameter.LXParameter;
 import heronarts.lx.pattern.LXPattern;
 import heronarts.lx.utils.LXUtils;
 
 @LXCategory("Nozzle")
 public class RingPattern extends LXPattern {
+    public enum ColorMode {
+        FIXED("Fixed"),
+        PALETTE("Palette");
+
+        public final String label;
+
+        private ColorMode(String label) {
+            this.label = label;
+        }
+
+        @Override
+        public String toString() {
+            return this.label;
+        }
+    }
+
     private static final int NUM_BANDS = 5;
+    public final EnumParameter<ColorMode> colorMode =
+            new EnumParameter<ColorMode>("Color Mode", ColorMode.FIXED)
+                    .setDescription("Which source the gradient selects colors from");
     public final ColorParameter color = new ColorParameter("Color").setDescription("Color of the pattern");
     public final CompoundParameter speed = new CompoundParameter("Speed", 1000, 500, 5000);
+    public final DiscreteParameter paletteIndex =
+            new DiscreteParameter("Index", 1, LXSwatch.MAX_COLORS + 1)
+                    .setDescription("Which index at the palette to start from");
 
     final SawLFO rotation = startModulator(new SawLFO(0, 360, speed));
 
@@ -23,24 +50,48 @@ public class RingPattern extends LXPattern {
         super(lx);
         this.color.setColor(color);
         this.speed.setUnits(LXParameter.Units.MILLISECONDS); // TODO: Hertz?
-        addParameter("color", this.color);
         addParameter("speed", this.speed);
+        addParameter("color", this.color);
+        addParameter("colorMode", this.colorMode);
+        addParameter("paletteIndex", this.paletteIndex);
     }
 
     public RingPattern(LX lx) {
         this(lx, LXColor.RED);
     }
 
+    public LXDynamicColor getPaletteColor() {
+        return this.lx.engine.palette.getSwatchColor(this.paletteIndex.getValuei() - 1);
+    }
+
     @Override
     protected void run(double deltaMs) {
+        float h;
+        float s;
+        switch (this.colorMode.getEnum()) {
+            case PALETTE:
+                h = (float)getPaletteColor().getHue();
+                s = (float)getPaletteColor().getSaturation();
+                break;
+            case FIXED:
+            default:
+                h = color.hue.getValuef();
+                s = color.saturation.getValuef();
+                break;
+        }
         double rot = rotation.getValue();
-        float offset = (float)(rot * (Math.PI/180));
-        for (LXPoint p : model.points) {
-            int band = (int)(p.zn / (1f / (float)(NUM_BANDS - 1)));
-            int sign = (band % 2 == 0) ? -1 : 1;
-            float b = Math.abs(LXUtils.sinf(((sign * offset) + p.theta) / 1f));
-            //b = Math.abs(LXUtils.trif((sign * offset) + (p.theta / LX.TWO_PIf)));
-            colors[p.index] = LX.hsb(color.hue.getValuef(), color.saturation.getValuef(), b * 100);
+        int sign = 1;
+        float offset = (float) (rot * (Math.PI / 180));
+        // TODO: access points through model.children of model.childDict so we know which ring we are in
+        for (LXModel model : this.model.children("nozzle")) {
+            for (LXPoint p : model.points) {
+                //int band = (int)(p.zn / (1f / (float)(NUM_BANDS - 1)));
+                //int sign = (band % 2 == 0) ? -1 : 1;
+                float b = Math.abs(LXUtils.sinf(((sign * offset) + p.theta)));
+                //b = Math.abs(LXUtils.trif((sign * offset) + (p.theta / LX.TWO_PIf)));
+                colors[p.index] = LX.hsb(h, s, b * 100);
+            }
+            sign = -sign;
         }
     }
 }
